ba56cbb26a41d1e6e17e16cad2e787de46def3ae
Ivan <sluvkann@polytechnic.poltava.ua>
2020-03-31 17:37:36 +0300

    Change dish model photo data

---
+++ CanteenFoodOrdering-Server/Migrations/20200331143544_ChangePhotoData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CanteenFoodOrdering_Server.Migrations
+{
+    public partial class ChangePhotoData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Photo",
+                table: "Dishes",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageMimeType",
+                table: "Dishes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageMimeType",
+                table: "Dishes");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Photo",
+                table: "Dishes",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
