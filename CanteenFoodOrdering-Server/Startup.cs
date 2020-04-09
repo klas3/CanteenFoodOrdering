512c52fc2d20c8bb346d73acb01d54b029f2088f
Ivan <sluvkann@polytechnic.poltava.ua>
2020-04-09 17:23:34 +0300

    Remove sending error details

--- CanteenFoodOrdering-Server/Startup.cs
+++ CanteenFoodOrdering-Server/Startup.cs
@@ -29,11 +29,6 @@ namespace CanteenFoodOrdering_Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR(options =>
-            {
-                options.EnableDetailedErrors = true;
-            });
-
             services.AddControllersWithViews();
 
             services.AddDbContext<Context>(options =>
