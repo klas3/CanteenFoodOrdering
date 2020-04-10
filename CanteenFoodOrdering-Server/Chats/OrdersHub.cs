b8e9c4793a7f29181bb19de9f45ea322158427bf
Ivan <sluvkann@polytechnic.poltava.ua>
2020-04-10 15:42:47 +0300

    Add hub context t resolve conflict

--- CanteenFoodOrdering-Server/Chats/OrdersHub.cs
+++ CanteenFoodOrdering-Server/Chats/OrdersHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,15 @@ namespace CanteenFoodOrdering_Server.Chats
 {
     public class OrdersHub : Hub
     {
-        public async Task SendToCashier(string order)
+        private IHubContext<OrdersHub> _context;
+
+        public OrdersHub(IHubContext<OrdersHub> context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Roles = "Customer")]
+        public async Task SendToCashier(object order)
         {
             await this.Clients.All.SendAsync("SendToCashier", order);
         }
